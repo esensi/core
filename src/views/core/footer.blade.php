adf90f9dc1ce54bbcb3f22887e76f45b42b1b054
bexarcreative-daniel <daniel@bexarcreative.com>
2014-01-09 16:36:49 -0600

    Added typeahead Basset collection.

--- src/views/core/footer.blade.php
+++ src/views/core/footer.blade.php
@@ -1,6 +1,6 @@
 	<div id="albaModal" role="dialog" aria-labelledby="albaModalLabel" aria-hidden="true" class="modal fade"></div>
 
-	@javascripts('jqueryui', 'bootstrap', 'application')
+	@javascripts('jqueryui', 'bootstrap', 'typeahead', 'application')
 
 </body>
 </html>

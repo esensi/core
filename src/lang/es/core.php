a4382d38bf397f51a871514eb0f9e90e04562026
bexarcreative-daniel <daniel@bexarcreative.com>
2014-02-04 10:25:28 -0600

    Added Spanish language files: translation still needed.

---
+++ src/lang/es/core.php
@@ -0,0 +1,41 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Error messages
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the default error messages used by
+	| the Core module.
+	|
+	*/
+
+	'errors' => [
+		'store' => 'Object could not be stored.',
+		'show' => 'Object could not be found.',
+		'update' => 'Object could not be updated.',
+		'restore' => 'Object could not be restored.',
+		'destroy' => 'Object could not be deleted.',
+		'trashing' => 'Object cannot be sent to trash.',
+	],
+
+
+	/*
+	|--------------------------------------------------------------------------
+	| Status message lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain status message lines used by the
+	| Core module models.
+	|
+	*/
+
+	'messages' => [
+		'never_expires'	=> 'Never Expires',
+		'never_updated'	=> 'Never Updated',
+		'never_created' => 'Never Created',
+		'never_deleted' => 'Never Deleted',
+	],
+];
---
+++ src/lang/es/role.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Error messages
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the default error messages used by
+	| the Role module.
+	|
+	*/
+
+	'errors' => [
+		'show'								=> 'Role could not be found.',
+		'show_by_name'						=> 'Role could not be found matching this name.',
+		'store'								=> 'Role could not be created.',
+		'update'							=> 'Role could not be saved.',
+		'destroy'							=> 'Role could not be deleted.',
+		'no_results'						=> 'There are no role results.',
+	],
+
+	/*
+	|--------------------------------------------------------------------------
+	| Success messages
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines contain the default success messages used by
+	| the Role module.
+	|
+	*/
+
+	'success' => [
+		'destroy'							=> 'Role has been deleted.',
+		'store'								=> 'Role has been created.',
+		'update'							=> 'Role has been saved.',
+	],
+	
+];

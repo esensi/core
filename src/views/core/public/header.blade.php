8480d519f2affaf69a49d7384e62f8de9180449e
bexarcreative-daniel <daniel@bexarcreative.com>
2014-05-07 20:02:23 +0000

    Renamed window title to Esensi.

--- src/views/core/public/header.blade.php
+++ src/views/core/public/header.blade.php
@@ -9,7 +9,7 @@
 		<meta name="author" content="">
 		<meta name="generator" content="{{ gethostname() }}">
 		<link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
-		<title>Title</title>
+		<title>Esensi</title>
 		
 		@styles()
 		

55fffbb214be222dc2a3f60aaf2dbcc65fbfabec
dalabarge <daniel@bexarcreative.com>
2014-10-28 22:35:08 -0500

    Tweak text on missing page.

--- src/views/core/public/missing.blade.php
+++ src/views/core/public/missing.blade.php
@@ -10,7 +10,7 @@
           <h3 class="panel-title text-center">Page Not Found</h3>
         </div>
         <div class="panel-body">
-          <p>It looks like you have gotten lost and wound up at a missing page. Please point yourself <a href="{{ route('index') }}">back to the home page</a> and find your way. If you think you've received this message in error please <a href="mailto:{{ Config::get('mail.from.address') }}">contact us to report this error.</a></p>
+          <p>It looks like you have gotten lost and wound up at a missing page. Please point yourself <a href="{{ route('index') }}">back to the home page</a> and find your way from here. If you think you've received this message in error please <a href="mailto:{{ Config::get('mail.from.address') }}">contact us to report this error.</a></p>
         </div>
       </div>
       <ul class="nav nav-pills nav-justified">

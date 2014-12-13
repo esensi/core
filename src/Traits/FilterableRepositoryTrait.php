a9fc06aa19966f667881ce275e86aac564185815
dalabarge <daniel@bexarcreative.com>
2014-12-13 16:27:52 -0600

    Fixed typo.

--- src/Traits/FilterableRepositoryTrait.php
+++ src/Traits/FilterableRepositoryTrait.php
@@ -121,7 +121,7 @@ trait FilterableRepositoryTrait {
     protected function filterTrashed($query)
     {
         // Enable filter if model support it
-        if ( method_exists($this->getModel(), 'softDelete') && isset($this->trashed) )
+        if ( method_exists($this->getModel(), 'forceDelete') && isset($this->trashed) )
         {
             // Only include the trashed results
             if( (string) $this->trashed == 'only' )

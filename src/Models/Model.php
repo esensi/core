614201043760f428f5a3db896a66dc9d259045f7
dalabarge <daniel@bexarcreative.com>
2015-06-23 00:49:50 -0500

    Fixed compatibility bugs.

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -209,7 +209,7 @@ class Model extends BaseModel {
     public function scopeListAlphabetically($query, $column = null, $key = null, $sort = 'asc')
     {
         $column = is_null($column) ? $this->getKeyName() : $column;
-        return $query->orderBy($column, $sort)->lists($column, $key);
+        return $query->orderBy($column, $sort)->lists($column, $key)->all();
     }
 
 }

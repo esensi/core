6ddca42b1d34af2760253d9973290c0e1d70c12c
stygiansabyss <stygian.warlock.v2@gmail.com>
2015-10-07 11:32:30 -0500

    Updating RouteServiceProvider and adding stub

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -17,13 +17,30 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * This namespace is applied to the controller routes in your routes file.
+     * The namespaces is applied to all UI controller routes.
      *
      * In addition, it is set as the URL generator's root namespace.
      *
+     * @var array
+     */
+    protected $controllers = [
+        'admin'  => 'App\Http\Controllers\Admin',
+        'public' => 'App\Http\Controllers\Public',
+    ];
+
+    /**
+     * This namespace is applied to all API routes.
+     *
      * @var string
      */
-    protected $namespace = 'App\Http\Controllers';
+    protected $apis = 'App\Http\Apis';
+
+    /**
+     * Any custom patterns the package should set to avoid collisions.
+     *
+     * @var array
+     */
+    protected $patterns = [];
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -35,12 +52,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         parent::boot($router);
 
-        $router->pattern('id', '[0-9]+');
-        $router->pattern('ids', '[0-9\+]+');
-        $router->pattern('slug', '[a-zA-Z][a-zA-Z0-9\-\_]+');
-        $router->pattern('action', '[a-zA-Z][a-zA-Z0-9\-\_]+');
-        $router->pattern('token', '[a-zA-Z0-9]+');
-        $router->pattern('relationship', '[a-zA-Z0-9\_\-\+]+');
+        $this->bindPatterns($router);
     }
 
     /**
@@ -51,7 +63,54 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        //
+        $this->mapControllers($router);
+        $this->mapApis($router);
     }
 
+    /**
+     * Bind any patterns to avoid package collisions.
+     *
+     * @param \Illuminate\Routing\Router $router
+     */
+    private function bindPatterns(Router $router)
+    {
+        $router->pattern('id', '[0-9]+');
+        $router->pattern('ids', '[0-9\+]+');
+        $router->pattern('slug', '[a-zA-Z][a-zA-Z0-9\-\_]+');
+        $router->pattern('action', '[a-zA-Z][a-zA-Z0-9\-\_]+');
+        $router->pattern('token', '[a-zA-Z0-9]+');
+        $router->pattern('relationship', '[a-zA-Z0-9\_\-\+]+');
+
+        foreach ($this->patterns as $name => $pattern) {
+            $router->pattern($name, $pattern);
+        }
+    }
+
+    /**
+     * This namespace is applied to the controller routes in your routes file.
+     * Each entry expects a routes file with a name matching the key.
+     * ie: $this->controllers['admin'] expects Routes/admin.php.
+     *
+     * @param \Illuminate\Routing\Router $router
+     */
+    protected function mapControllers(Router $router)
+    {
+        foreach ($this->controllers as $ui => $namespace) {
+            $router->group(['namespace' => $namespace], function ($router) use ($ui) {
+                require_once __DIR__ . '/../Http/Routes/' . $ui . '.php';
+            });
+        }
+    }
+
+    /**
+     * Define the routes for the API.
+     *
+     * @param \Illuminate\Routing\Router $router
+     */
+    protected function mapApis(Router $router)
+    {
+        $router->group(['namespace' => $this->apis], function ($router) {
+            require_once __DIR__ . '/../Http/Routes/api.php';
+        });
+    }
 }

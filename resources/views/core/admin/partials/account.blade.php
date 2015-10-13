1e4fda21d6e302f0983e40d1110bb15b073ef4cb
dalabarge <daniel@bexarcreative.com>
2015-10-13 12:12:33 -0500

    Updated partials for bulk actions and account menu dropdowns.

--- resources/views/core/admin/partials/account.blade.php
+++ resources/views/core/admin/partials/account.blade.php
@@ -1,4 +1,4 @@
 <div class="account-menu-dropdown">
   <a class="account-menu-profile" href="{{ route('admin.users.account.edit') }}">Edit My Profile</a>
-  <a class="account-menu-logout" href="{{ route('users.logout') }}">Logout &rarr;</a>
+  <a class="account-menu-logout" href="{{ route('users.logout') }}">Logout</a>
 </div>
--- resources/views/core/admin/partials/bulk-actions.blade.php
+++ resources/views/core/admin/partials/bulk-actions.blade.php
@@ -1,18 +1,18 @@
 <div class="bulk-actions" data-toggle="bulk">
   @if( Route::currentRouteName() == 'admin.'. $package .'.dumpster' )
-    <a data-bulk-url="{{ route('admin.'. $package .'.restore.bulk.confirm', ':ids') }}" class="bulk-action" data-toggle="modal" data-target="#esensiModal">
+    <a data-bulk-url="{{ route('admin.'. $package .'.restore.bulk.confirm', ':ids') }}" class="bulk-restore" data-toggle="modal" data-target="#esensiModal">
       <span>Restore</span>
     </a>
-    <a data-bulk-url="{{ route('admin.'. $package .'.delete.bulk.confirm', ':ids') }}" class="bulk-action" data-toggle="modal" data-target="#esensiModal">
+    <a data-bulk-url="{{ route('admin.'. $package .'.delete.bulk.confirm', ':ids') }}" class="bulk-delete" data-toggle="modal" data-target="#esensiModal">
       <span>Delete</span>
     </a>
   @else
     @if( Route::has('admin.'. $package . '.dumpster') )
-      <a data-bulk-url="{{ route('admin.'. $package .'.trash.bulk.confirm', ':ids') }}" class="bulk-action" data-toggle="modal" data-target="#esensiModal">
+      <a data-bulk-url="{{ route('admin.'. $package .'.trash.bulk.confirm', ':ids') }}" class="bulk-trash" data-toggle="modal" data-target="#esensiModal">
         <span>Trash</span>
       </a>
     @else
-      <a data-bulk-url="{{ route('admin.'. $package .'.delete.bulk.confirm', ':ids') }}" class="bulk-action" data-toggle="modal" data-target="#esensiModal">
+      <a data-bulk-url="{{ route('admin.'. $package .'.delete.bulk.confirm', ':ids') }}" class="bulk-delete" data-toggle="modal" data-target="#esensiModal">
         <span>Delete</span>
       </a>
     @endif

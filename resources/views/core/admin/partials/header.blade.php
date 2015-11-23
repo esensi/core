52569176324a9be2012404c7f864b2f3df83cb2b
dalabarge <daniel@bexarcreative.com>
2015-11-23 16:38:45 -0600

    Added status code class to body elements in headers.

--- resources/views/core/admin/partials/header.blade.php
+++ resources/views/core/admin/partials/header.blade.php
@@ -20,7 +20,7 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body class="admin {{ str_replace('.', '-', Route::currentRouteName()) }}">
+  <body class="admin {{ str_replace('.', '-', Route::currentRouteName()) }} {{ isset($code) && $code !== 200 ? 'status-' . $code : null }}">
     <!--[if lt IE 9]>
       <div class="alert alert-info">
         <strong>Heads up!</strong> You're using an older web browser, so some parts of this site may not work properly.

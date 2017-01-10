a00337e942bacd70c250635e3fbcc44f5dbb53bd
Daniel LaBarge <daniel@artisanscollab.com>
2017-01-10 12:27:05 -0600

    Reorder modal button position for BS4 alpha6

--- resources/views/core/admin/modal.blade.php
+++ resources/views/core/admin/modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal-header">
-  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
   <h4 class="modal-title" id="esensiModalTitle">@yield('modal-title')</h4>
+  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 </div>
 <div class="modal-body">
   @include(config('esensi/core::core.partials.admin.errors'))

08672527f2256935b1316488f88f0d2d2087be14
Will Rowe <wrowe@will-rowe.com>
2023-05-25 12:54:16 -0400

    Add migration to remove indexes

---
+++ database/migrations/2023_05_25_124948_remove_indexes_from_failed_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->dropIndex(['connection']);
+            $table->dropIndex(['queue']);
+            $table->dropIndex(['failed_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->index('connection');
+            $table->index('queue');
+            $table->index('failed_at');
+        });
+    }
+};
